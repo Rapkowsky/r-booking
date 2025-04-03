61c76d1c106e9782cc7f4785970fec4ee719f328
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-04-03 16:48:17 +0200

    fix: @ password layout

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -47,8 +47,12 @@ const SignInForm = () => {
         <h2 className='text-center text-2xl'>Sign In</h2>
         <p className='text-center text-muted-foreground'>
           Sign in using the following credentials:
-          <span className='block'>Email: demo&#64;cosdensolutions.io</span>
-          <span className='block'>Password: cosdensolutions</span>
+        </p>
+        <p className='mb-0 text-center text-muted-foreground'>
+          Email: demo&#64;cosdensolutions.io
+        </p>
+        <p className='text-center text-muted-foreground'>
+          Password: cosdensolutions
         </p>
         <Separator />
       </CardHeader>

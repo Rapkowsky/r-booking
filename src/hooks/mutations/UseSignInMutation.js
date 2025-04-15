6dab8a2cd69e82042d8e4bfde090890b15c3eef4
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-04-15 16:55:19 +0200

    rename UseSignInMutation to useSignInMutation to follow naming conventions

--- src/hooks/mutations/UseSignInMutation.js
+++ src/hooks/mutations/UseSignInMutation.js
@@ -2,10 +2,10 @@ import { useMutation } from '@tanstack/react-query';
 
 import api from '@/api';
 
-const UseSignInMutation = () => {
+const useSignInMutation = () => {
   return useMutation({
     mutationFn: (data) => api.post('/api/signin', data),
   });
 };
 
-export default UseSignInMutation;
+export default useSignInMutation;

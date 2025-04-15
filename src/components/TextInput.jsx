ecedbbc6398cd24d324080d98d08701adf71c600
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-04-15 16:54:18 +0200

    enhance TextInput component with multiline support

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,16 +1,22 @@
+import { useId } from 'react';
 import { useController } from 'react-hook-form';
 
-import { Input } from './ui';
+import { Input } from '@/components/ui';
+import { TextArea } from '@/components/ui/TextArea';
 
-const TextInput = ({ control, name, type = 'text', ...rest }) => {
+const TextInput = ({ control, multiline, name, type = 'text', ...rest }) => {
   const form = useController({ control, name });
+
   const error = form.formState.errors[name];
 
+  const Component = multiline ? TextArea : Input;
+
   return (
-    <div>
-      <Input
-        type={type}
+    <div className='flex flex-col gap-2'>
+      <Component
         {...rest}
+        type={type}
+        name={name}
         onChange={form.field.onChange}
         onBlur={form.field.onBlur}
         value={form.field.value || ''}

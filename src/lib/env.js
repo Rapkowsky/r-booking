3f0ef5b77f67a81b8f04b60bca7f3175d9ac8a32
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-04-15 16:53:03 +0200

    remove unused COURSE_URL environment variable

--- src/lib/env.js
+++ src/lib/env.js
@@ -2,7 +2,6 @@ import * as z from 'zod';
 
 const envSchema = z.object({
   BASE_URL: z.string().url(),
-  COURSE_URL: z.string().url(),
   COSDEN_SOLUTIONS_URL: z.string().url(),
   DB_KEY: z.string(),
   DISCORD_URL: z.string().url(),
@@ -11,7 +10,6 @@ const envSchema = z.object({
 
 export const env = envSchema.parse({
   BASE_URL: import.meta.env.VITE_BASE_URL,
-  COURSE_URL: import.meta.env.VITE_COURSE_URL,
   COSDEN_SOLUTIONS_URL: import.meta.env.VITE_COSDEN_SOLUTIONS_URL,
   DB_KEY: import.meta.env.VITE_DB_KEY,
   DISCORD_URL: import.meta.env.VITE_DISCORD_URL,
